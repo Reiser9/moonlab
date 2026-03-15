2d2eefa32aac561b9aeab40548604fefc0aaf01f
xw1nchester <vetroveg.72@mail.ru>
2026-03-15 13:03:26 +0500

    testing cd

--- app/terminal/data.ts
+++ app/terminal/data.ts
@@ -1,4 +1,4 @@
-import { CandlestickData, Time } from "lightweight-charts";
+import { CandlestickData, Time } from 'lightweight-charts';
 
 export const initialDataChart: CandlestickData<Time>[] = [
     {
@@ -6,349 +6,349 @@ export const initialDataChart: CandlestickData<Time>[] = [
         low: 187.19,
         high: 210.9,
         open: 192.61,
-        close: 189.83,
+        close: 189.83
     },
     {
         time: 1510488000 as Time,
         low: 174.17,
         high: 199.9,
         open: 193.52,
-        close: 188.74,
+        close: 188.74
     },
     {
         time: 1510574400 as Time,
         low: 167.49,
         high: 187.79,
         open: 181.44,
-        close: 178.85,
+        close: 178.85
     },
     {
         time: 1510660800 as Time,
         low: 156.28,
         high: 185.35,
         open: 179.54,
-        close: 172.47,
+        close: 172.47
     },
     {
         time: 1510747200 as Time,
         low: 165.81,
         high: 187.42,
         open: 172.94,
-        close: 169.38,
+        close: 169.38
     },
     {
         time: 1510833600 as Time,
         low: 165.44,
         high: 179.59,
         open: 168.27,
-        close: 170.78,
+        close: 170.78
     },
     {
         time: 1510920000 as Time,
         low: 161.74,
         high: 189.81,
         open: 174.96,
-        close: 182.89,
+        close: 182.89
     },
     {
         time: 1511006400 as Time,
         low: 173.11,
         high: 207.27,
         open: 187.08,
-        close: 191.71,
+        close: 191.71
     },
     {
         time: 1511092800 as Time,
         low: 180.54,
         high: 197.68,
         open: 184.73,
-        close: 180.96,
+        close: 180.96
     },
     {
         time: 1511179200 as Time,
         low: 178.35,
         high: 198.29,
         open: 186.68,
-        close: 193.49,
+        close: 193.49
     },
     {
         time: 1511265600 as Time,
         low: 177.31,
         high: 209.82,
         open: 198.23,
-        close: 195.16,
+        close: 195.16
     },
     {
         time: 1511352000 as Time,
         low: 175.18,
         high: 210.45,
         open: 195.54,
-        close: 193.23,
+        close: 193.23
     },
     {
         time: 1511438400 as Time,
         low: 177.99,
         high: 188.37,
         open: 186.3,
-        close: 186.79,
+        close: 186.79
     },
     {
         time: 1511524800 as Time,
         low: 179.81,
         high: 199.53,
         open: 193.9,
-        close: 195.22,
+        close: 195.22
     },
     {
         time: 1511611200 as Time,
         low: 177.3,
         high: 204.57,
         open: 188.43,
-        close: 188.58,
+        close: 188.58
     },
     {
         time: 1511697600 as Time,
         low: 177.87,
         high: 196.29,
         open: 183.89,
-        close: 185.98,
+        close: 185.98
     },
     {
         time: 1511784000 as Time,
         low: 168.61,
         high: 184.97,
         open: 180.43,
-        close: 171.73,
+        close: 171.73
     },
     {
         time: 1511870400 as Time,
         low: 163.13,
         high: 178.26,
         open: 175.89,
-        close: 173.32,
+        close: 173.32
     },
     {
         time: 1511956800 as Time,
         low: 168.27,
         high: 185.54,
         open: 177.47,
-        close: 182.68,
+        close: 182.68
     },
     {
         time: 1512043200 as Time,
         low: 173.37,
         high: 199.42,
         open: 174.07,
-        close: 182.04,
+        close: 182.04
     },
     {
         time: 1512129600 as Time,
         low: 168.87,
         high: 181.41,
         open: 178.27,
-        close: 170.35,
+        close: 170.35
     },
     {
         time: 1512216000 as Time,
         low: 163,
         high: 188.37,
         open: 172.45,
-        close: 166.3,
+        close: 166.3
     },
     {
         time: 1512302400 as Time,
         low: 156.81,
         high: 165.94,
         open: 163.11,
-        close: 158.43,
+        close: 158.43
     },
     {
         time: 1512388800 as Time,
         low: 153.6,
         high: 179.18,
         open: 160.82,
-        close: 164.68,
+        close: 164.68
     },
     {
         time: 1512475200 as Time,
         low: 147.88,
         high: 164.98,
         open: 158.1,
-        close: 155.48,
+        close: 155.48
     },
     {
         time: 1512561600 as Time,
         low: 144.34,
         high: 155.6,
         open: 148.72,
-        close: 149.52,
+        close: 149.52
     },
     {
         time: 1512648000 as Time,
         low: 143.66,
         high: 155.31,
         open: 150.19,
-        close: 144.82,
+        close: 144.82
     },
     {
         time: 1512734400 as Time,
         low: 134.62,
         high: 154.92,
         open: 149.95,
-        close: 148.05,
+        close: 148.05
     },
     {
         time: 1512820800 as Time,
         low: 152.66,
         high: 171.15,
         open: 153.38,
-        close: 159.66,
+        close: 159.66
     },
     {
         time: 1512907200 as Time,
         low: 157.06,
         high: 177.41,
         open: 161.23,
-        close: 165.41,
+        close: 165.41
     },
     {
         time: 1512993600 as Time,
         low: 142.96,
         high: 166.79,
         open: 159.65,
-        close: 157.67,
+        close: 157.67
     },
     {
         time: 1513080000 as Time,
         low: 140.21,
         high: 169.82,
         open: 157.34,
-        close: 150.38,
+        close: 150.38
     },
     {
         time: 1513166400 as Time,
         low: 140.44,
         high: 156.26,
         open: 152.43,
-        close: 156.19,
+        close: 156.19
     },
     {
         time: 1513252800 as Time,
         low: 134.66,
         high: 165.84,
         open: 153.06,
-        close: 146.95,
+        close: 146.95
     },
     {
         time: 1513339200 as Time,
         low: 120.9,
         high: 151.08,
         open: 139.91,
-        close: 133.83,
+        close: 133.83
     },
     {
         time: 1513425600 as Time,
         low: 128.56,
         high: 157.5,
         open: 140.18,
-        close: 146.29,
+        close: 146.29
     },
     {
         time: 1513512000 as Time,
         low: 139.81,
         high: 156.09,
         open: 148.72,
-        close: 145.42,
+        close: 145.42
     },
     {
         time: 1513598400 as Time,
         low: 141.61,
         high: 171.16,
         open: 151.89,
-        close: 155.67,
+        close: 155.67
     },
     {
         time: 1513684800 as Time,
         low: 152.32,
         high: 166.54,
         open: 157.33,
-        close: 157.09,
+        close: 157.09
     },
     {
         time: 1513771200 as Time,
         low: 137.42,
         high: 164.58,
         open: 151.88,
-        close: 147.54,
+        close: 147.54
     },
     {
         time: 1513857600 as Time,
         low: 146.64,
         high: 157.67,
         open: 148.87,
-        close: 148.13,
+        close: 148.13
     },
     {
         time: 1513944000 as Time,
         low: 141.64,
         high: 149.07,
         open: 147.04,
-        close: 141.67,
+        close: 141.67
     },
     {
         time: 1514030400 as Time,
         low: 135.42,
         high: 153.16,
         open: 141.94,
-        close: 139.08,
+        close: 139.08
     },
     {
         time: 1514116800 as Time,
         low: 124.18,
         high: 147.45,
         open: 135.13,
-        close: 135.31,
+        close: 135.31
     },
     {
         time: 1514203200 as Time,
         low: 130.83,
         high: 155.43,
         open: 136.94,
-        close: 142,
+        close: 142
     },
     {
         time: 1514289600 as Time,
         low: 129.82,
         high: 149,
         open: 142.82,
-        close: 145.09,
+        close: 145.09
     },
     {
         time: 1514376000 as Time,
         low: 122.26,
         high: 142.25,
         open: 138.17,
-        close: 134.05,
+        close: 134.05
     },
     {
         time: 1514462400 as Time,
         low: 135.69,
         high: 150.33,
         open: 139.55,
-        close: 142.88,
+        close: 142.88
     },
     {
         time: 1514548800 as Time,
         low: 128.49,
         high: 152.33,
         open: 142.41,
-        close: 136.63,
+        close: 136.63
     },
     {
         time: 1514635200 as Time,
         low: 133.89,
         high: 146.81,
         open: 134.88,
-        close: 141.49,
-    },
+        close: 141.49
+    }
 ];
